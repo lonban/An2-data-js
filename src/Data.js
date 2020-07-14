60acb37a3c81240429085d4b6dc25e160b11e376
龙邦 <antietie@qq.com>
2020-07-14 16:48:07 +0800

    优化缓存，添加用户区分user_id，添加时间过期自动删除功能，向后新增方法优化（当存储长度到达最大长度后，把存储的内容删除一半）

--- src/Data.js
+++ src/Data.js
@@ -17,12 +17,25 @@ class Data{
     this.axios = Axios.create({
       baseURL: this.api, //基础url前缀
       headers: {'X-Requested-With': 'XMLHttpRequest','Content-Type': 'application/x-www-form-urlencoded'}, //请求头信息XMLHttpRequest是Ajax异步请求方式，为null则传统同步请求
-      timeout: 5000, //设置超时时间
+      timeout: 30000, //设置超时时间
       responseType: 'json', //返回数据类型,默认也是json
       transformRequest: [function (data){
         return Qs.stringify(data) //对json数据转成&字符拼接的形式
       }],
     });
+  }
+
+  setAuth_(access_token)
+  {
+    if(access_token){
+      this.axios.interceptors.request.use(function(config){
+        /*会生成一个Authorization: Bearer token头信息*/
+        config.headers['Authorization'] = 'Bearer '+access_token;
+        return config;
+      },function(error){
+        return Promise.reject(error);
+      });
+    }
   }
 
   /*地址方法*/
@@ -35,35 +48,293 @@ class Data{
     });
   }
 
+  /*注册*/
+  createRegister(data)
+  {
+    data = {
+      'avatarurl':data.photo,
+      'nickname':data.name,
+      'sex':data.sex,
+      'location':data.location,
+      'isencryp':data.isencryp,
+      'agelevel':data.age,
+      'purpose':data.purpose,
+      'userid':this.this__.$store.state.loginUser.userid
+    }
+    return new Promise((resolve)=>{
+      this.axios.post('/applogin/refreshinfo.php',data).then(res=>{
+        resolve(res.data);
+      });
+    });
+  }
+
   /*登录*/
   login()
   {
-    let this_ = this.this__;
     return new Promise((resolve)=>{
-      let token  = this_.$An2_Link.get('access_token'); //获取token
-      this.axios.post('applogin/loginfo2.php', {},{
+      let token  = this.this__.$An2_Link.get('access_token'); //获取token
+      this.axios.post('/applogin/loginfo2.php', {},{
         headers:{access_token: token}
       }).then(res => {
         let data = res.data;
         if (data.returncode == 0) {
-          this_.$store.commit('login',data.loginUser);
+          data.loginUser.sex = parseInt(data.loginUser.sex);
+          this.this__.$store.commit('login',data.loginUser);
         } else {
-          this_.$store.commit('login', {});
+          this.this__.$store.commit('login', {});
         }
         resolve(data);
       });
     });
   }
 
-  /*注册*/
-  createRegister(data)
+  /*获取所有用户信息*/
+  getUserAll()
   {
-    let this_ = this.this__;
-    data['userid'] = this_.$store.state.loginUser.userid;
     return new Promise((resolve)=>{
-      this.axios.post('/applogin/refreshinfo.php',data).then(res=>{
-        resolve(res.data);
+      let res_data = [];
+      this.axios.get('/apphome/loadhomerecommed').then(res=>{
+        res = res.data.recommonlist;
+        for(let k in res){
+          res_data[k] = {};
+          if(parseInt(res[k]['status'])&&parseInt(res[k]['iscompleted'])){
+            res_data[k]['id'] = res[k]['userid'];
+            res_data[k]['photo'] = res[k]['headimg']||"../load.png"; //头像
+            res_data[k]['name'] = res[k]['nickname']; //昵称
+            res_data[k]['sex'] = parseInt(res[k]['sex']); //性别
+            res_data[k]['age'] = res[k]['agelevel']; //年龄
+            res_data[k]['location'] = res[k]['currentcity']||'无法获取'; //地址
+            if(res[k]['purpose']){ //交友目地
+              res_data[k]['interest'] = ['恋爱', '旅游', '望海'];//res.data.recommonlist[k]['purpose'].split(',');
+            }else{
+              res_data[k]['interest'] = [];
+            }
+            res_data[k]['signature'] = '等一个你陪我度过等一个你陪我度过等一个你陪我度过等一个你陪我度过等一个你陪我度过'||'...'; //个性签名
+          }
+        }
+        resolve(res_data);
       });
+    });
+  }
+
+  /*通过用户id获取用户信息*/
+  getUser(user_id)
+  {
+    let res_data ={
+      'id':10000039,
+      'photo':'http://s3.2u.chat.s3-us-west-1.amazonaws.com/data/www/resources/avatar/t/39/10000039.jpg',
+      'name':'吴彦祖',
+      'sex':1,
+      'age':45,
+      'location':'无法获取',
+      'purpose':'杨茜茜,花木成畦手自栽,模压,啊沙发上',
+      'interest':['恋爱', '旅游', '望海'],
+      'signature':'等一个你陪我度过等一个你陪我度过等一个你陪我度过等一个你陪我度过等一个你陪我度过',
+    };
+    return new Promise((resolve)=>{
+      return resolve(res_data);
+    });
+  }
+
+  /*通过类型id获取类型信息*/
+  getType(type_id)
+  {
+    let res_data = [
+      {
+        'id':1,
+        'status':1,
+        'name':'文艺青年',
+        'description':'',
+      },
+      {
+        'id':2,
+        'status':1,
+        'name':'娱乐人生',
+        'description':'',
+      },
+      {
+        'id':3,
+        'status':1,
+        'name':'品质生活',
+        'description':'',
+      },
+    ];
+    let data = {};
+    for(let i in res_data){
+      if(res_data[i]['id'] == type_id){
+        data = res_data[i];
+        break;
+      }
+    }
+    return new Promise((resolve)=>{
+      return resolve(data);
+    });
+  }
+
+  /*通过多媒体id获取多媒体信息*/
+  getMedia(media_id)
+  {
+    let res_data = [
+      {
+        'id':1,
+        'status':1,
+        'type':'img',
+        'address':'http://s3.2u.chat.s3-us-west-1.amazonaws.com/data/www/resources/avatar/t/23/10000023.jpg',
+        'length':20,
+      },
+      {
+        'id':2,
+        'status':1,
+        'type':'music',
+        'address':'http://music.163.com/song/media/outer/url?id=447925558.mp3',
+        'length':30,
+      },
+      {
+        'id':3,
+        'status':1,
+        'type':'music',
+        'address':'http://up_mp4.t57.cn/2018/1/03m/13/396131227319.m4a',
+        'length':30,
+      },
+      {
+        'id':4,
+        'status':1,
+        'type':'video',
+        'address':'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+        'length':40,
+      },
+      {
+        'id':5,
+        'status':1,
+        'type':'video',
+        'address':'http://vjs.zencdn.net/v/oceans.mp4',
+        'length':40,
+      },
+      {
+        'id':6,
+        'status':1,
+        'type':'video',
+        'address':'http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4',
+        'length':40,
+      },
+    ];
+    let data = {};
+    for(let i in res_data){
+      if(res_data[i]['id'] == media_id){
+        data = res_data[i];
+        break;
+      }
+    }
+    return new Promise((resolve)=>{
+      return resolve(data);
+    });
+  }
+
+  /*获取指定类型的动态信息*/
+  getCommunity(type_id)
+  {
+    let res_data = [
+      {
+        'userid':'10000040',
+        'type_id':4,
+        'media_id':1,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+      {
+        'userid':'10000040',
+        'type_id':5,
+        'media_id':2,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，鸡蛋酱拌米粉，鸡蛋酱拌米粉，鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+      {
+        'userid':'10000040',
+        'type_id':5,
+        'media_id':3,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+      {
+        'userid':'10000040',
+        'type_id':1,
+        'media_id':4,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+      {
+        'userid':'10000040',
+        'type_id':1,
+        'media_id':5,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+      {
+        'userid':'10000040',
+        'type_id':1,
+        'media_id':6,
+        'title':'',
+        'content':'鸡蛋酱拌米粉，简单的美味，不失营养',
+        'is_money':0,
+        'media_money':10,
+        'clock_money':40,
+        'white':'',
+        'black':'10000039,10000040',
+        'like':'666',
+        'ranking':'100'
+      },
+    ];
+    return new Promise((resolve)=>{
+      for(let i in res_data){
+        this.getMedia(res_data[i]['media_id']).then(res=>{
+          delete res_data[i]['media_id'];
+          res_data[i]['media'] = res;
+        });
+        /*this.getType(res_data[i]['type_id']).then(res=>{
+          delete res_data[i]['type_id'];
+          res_data[i]['type'] = res;
+        });*/
+        this.getUser(res_data[i]['userid']).then(res=>{
+          delete res_data[i]['userid'];
+          res_data[i]['user'] = res;
+        });
+      }
+      return resolve(res_data);
     });
   }
 }
--- src/Local.js
+++ src/Local.js
@@ -1,46 +1,114 @@
-class Local
-{
-    /*创建本地缓存*/
-    createStorage(name,data)
-    {
-      switch(Object.prototype.toString.call(data)){
-        case '[object Object]':data = JSON.stringify(data);//对象
-          break;
-        case '[object Array]':data = JSON.stringify(data);//数组
-          break;
-      }
-      return localStorage.setItem('an_cc'+name,data);
-    }
+class Local {
 
-    addStorage(name,data)
-    {
-      switch(Object.prototype.toString.call(data)){
-        case '[object Object]':
-          data = JSON.stringify(data);
-          break;
-        case '[object Array]':
-          data = JSON.stringify(data);
-          break;
-      }
-      let data2 = localStorage.getItem('an_cc'+name);
-      if(data2){
-        return localStorage.setItem('an_cc'+name,data2+'_;_'+data);
-      }else{
-        return localStorage.setItem('an_cc'+name,data);
-      }
-    }
+  name_='an_';
+  name_is_=0;
+  max_length_=1500;
+  separator_='_;_';
 
-    getStorage(name,id)
-    {
-      let data = localStorage.getItem('an_cc'+name);
-      if(data) {
-        if (data.indexOf('_;_') != -1) {
-          data = data.split('_;_');
+  user(user_id)
+  {
+    if(user_id){
+      this.name_ += user_id;
+    }
+    this.name_is_ = 1;
+  }
+
+  callback_()
+  {
+    return new Promise((resolve)=>{
+      let interval_i = 0;
+      let interval = setInterval(()=>{
+        if(this.name_is_){
+          clearInterval(interval);
+          resolve(1);
+        }else{
+          if(interval_i++>20){
+            this.name_is_ = 1;
+          }
+        }
+      },200);
+    });
+  }
+
+  /*创建本地缓存*/
+  createStorage_(name, data, time) {
+    switch (Object.prototype.toString.call(data)) {
+      case '[object Object]':
+        data = JSON.stringify(data);//对象
+        break;
+      case '[object Array]':
+        data = JSON.stringify(data);//数组
+        break;
+    }
+    if (time) {
+      localStorage.setItem(this.name_ + 'time' + name, parseInt(Date.now()/1000)+time);
+    }
+    return localStorage.setItem(this.name_ + name, data);
+  }
+
+  createStorage(name, data, time)
+  {
+    return new Promise((resolve)=>{
+      this.callback_().then(res=>{
+        if(res){
+          resolve(this.createStorage_(name, data, time));
+        }
+      });
+    });
+  }
+
+  /*在原数据基础上从后面追加*/
+  addStorage_(name, data, time) {
+    switch (Object.prototype.toString.call(data)) {
+      case '[object Object]':
+        data = JSON.stringify(data);
+        break;
+      case '[object Array]':
+        data = JSON.stringify(data);
+        break;
+    }
+    let data2 = localStorage.getItem(this.name_ + name);
+    if(time){localStorage.setItem(this.name_ + 'time' + name, parseInt(Date.now()/1000)+time);}
+    if (data2) {
+      if(data2.length>this.max_length_){
+        data2 = data2.substr(parseInt(this.max_length_/2)).replace(/^_;_|^;_|^_/g,'');
+      }
+      return localStorage.setItem(this.name_ + name, data2 + this.separator_ + data);
+    } else {
+      return localStorage.setItem(this.name_ + name, data);
+    }
+  }
+
+  addStorage(name, data, time){
+    return new Promise((resolve)=>{
+      this.callback_().then(res=>{
+        if(res){
+          resolve(this.addStorage_(name, data, time));
+        }
+      });
+    });
+  }
+
+  getStorage_(name, id) {
+    let time = localStorage.getItem(this.name_ + 'time' + name);
+    if(time&&parseInt(time)<parseInt(Date.now()/1000)){
+      this.deleteStorage(name);
+      return '';
+    }else{
+      let data = localStorage.getItem(this.name_ + name);
+      if (data) {
+        if (data.indexOf(this.separator_) != -1) {
+          data = data.split(this.separator_);
           for (let i = 0; i < data.length; i++) {
             if (data[i].indexOf('{') != -1) {
               data[i] = JSON.parse(data[i]);
-              if (id) {
-                if (data[i]['id'] == id) {
+              if(Object.prototype.toString.call(id) == '[object Object]') {
+                let key = Object.keys(id)[0];
+                if (data[i][key] == id[key]) {
+                  return data[i];
+                }
+              }else if(Object.prototype.toString.call(id) == '[object Number]'){
+                if (i == id) {
                   return data[i];
                 }
               }
@@ -48,15 +116,56 @@ class Local
           }
         } else if (data.indexOf('{') != -1) {
           data = JSON.parse(data);
+          if(Object.prototype.toString.call(id) == '[object Number]'){
+            return data[id];
+          }
         }
       }
       return data;
     }
+  }
 
-    deleteStorage(name)
-    {
-      return localStorage.removeItem('an_cc'+name);
+  getStorage(name, id)
+  {
+    return new Promise((resolve)=>{
+      this.callback_().then(res=>{
+        if(res){
+          resolve(this.getStorage_(name, id));
+        }
+      });
+    });
+  }
+
+  deleteStorage_(name,id) {
+    let data = localStorage.getItem(this.name_ + name);
+    if (data) {
+      if(Object.prototype.toString.call(id) == '[object Number]'){
+        let str_i = data.indexOf(this.separator_);
+        if (str_i != -1) {
+          data = data.split(this.separator_);
+          data.splice(id,1);
+          return localStorage.setItem(this.name_ + name, data.join(this.separator_));
+        }else{
+          data = JSON.parse(data);
+          data.splice(id,1);
+          return localStorage.setItem(this.name_ + name, JSON.stringify(data));
+        }
+      }else{
+        return localStorage.removeItem(this.name_ + name);
+      }
     }
+  }
+
+  deleteStorage(name,id)
+  {
+    return new Promise((resolve)=>{
+      this.callback_().then(res=>{
+        if(res){
+          resolve(this.deleteStorage_(name,id));
+        }
+      });
+    });
+  }
 }
 
 export default Local;
